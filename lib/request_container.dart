634db4ed78d4803ecc9ad061fa2da2b12e2fbb87
Benjamin Quinn <bquinn1992@gmail.com>
2020-06-14 13:16:37 -0400

    Sticky header in request editor.

--- lib/request_container.dart
+++ lib/request_container.dart
@@ -2,8 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:repose/bloc/active_requests_bloc.dart';
-import 'package:repose/widgets/key_value_table.dart';
 import 'package:repose/widgets/dynamic_tab_view.dart';
+import 'package:repose/widgets/key_value_table.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 
 import 'bloc/models.dart';
 
@@ -20,8 +21,6 @@ class _RequestContainerState extends State<RequestContainer> with TickerProvider
   final _nameTextController = TextEditingController();
   final _requestEditingController = TextEditingController();
   final _requestEditingScrollController = ScrollController();
-  final _requestEditorScrollController = ScrollController();
-  TabController _paramHeaderTabController;
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +42,6 @@ class _RequestContainerState extends State<RequestContainer> with TickerProvider
     // Initialize controller state
     _urlTextController.text = bloc.state.activeRequest.url;
     _nameTextController.text = bloc.state.activeRequest.name;
-    _paramHeaderTabController = TabController(length: 2, vsync: this);
 
     var theme = Theme.of(context);
 
@@ -58,8 +56,8 @@ class _RequestContainerState extends State<RequestContainer> with TickerProvider
       builder: (context, state) => Expanded(
         child: state.activeRequest == null
             ? Text('No request selected')
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+            : ListView(
+                controller: _requestEditingScrollController,
                 children: [
                   TextField(
                     decoration: InputDecoration(hintText: 'Request Name'),
@@ -67,43 +65,55 @@ class _RequestContainerState extends State<RequestContainer> with TickerProvider
                     onChanged: (val) => bloc.add(NameChangedEvent(val)),
                     onSubmitted: (val) => bloc.add(RequestNameSubmittedEvent(state.activeRequest)),
                   ),
-                  buildRequestInfoBar(state, bloc),
-                  buildRequestEditor(state, theme),
-                  buildResponseText(state),
+                  StickyHeader(
+                    header: buildRequestInfoBar(state, bloc, theme),
+                    content: SizedBox(
+                      height: 1000,
+                      child: Column(
+                        children: [
+                          buildRequestEditor(state, theme),
+                          buildResponseText(state),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
       ),
     );
   }
 
-  Widget buildRequestInfoBar(ActiveRequestsListState state, ActiveRequestsListBloc bloc) {
-    return Row(
-      children: [
-        DropdownButton(
-          value: state.activeRequest.method,
-          items: HttpMethod.values
-              .map((e) => DropdownMenuItem(
-            value: e,
-            child: Text(e.toString().split('.').last),
-          ))
-              .toList(),
-          onChanged: (val) => bloc.add(MethodChangedEvent(val)),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-            child: TextField(
-              decoration: InputDecoration(hintText: 'Url'),
-              controller: _urlTextController,
-              onChanged: (val) => bloc.add(UrlChangedEvent(val)),
+  Widget buildRequestInfoBar(ActiveRequestsListState state, ActiveRequestsListBloc bloc, ThemeData theme) {
+    return Container(
+      color: theme.scaffoldBackgroundColor,
+      child: Row(
+        children: [
+          DropdownButton(
+            value: state.activeRequest.method,
+            items: HttpMethod.values
+                .map((e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e.toString().split('.').last),
+                    ))
+                .toList(),
+            onChanged: (val) => bloc.add(MethodChangedEvent(val)),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+              child: TextField(
+                decoration: InputDecoration(hintText: 'Url'),
+                controller: _urlTextController,
+                onChanged: (val) => bloc.add(UrlChangedEvent(val)),
+              ),
             ),
           ),
-        ),
-        FlatButton(
-          child: Text('Send Request'),
-          onPressed: () => bloc.add(InitiateRequestEvent()),
-        ),
-      ],
+          FlatButton(
+            child: Text('Send Request'),
+            onPressed: () => bloc.add(InitiateRequestEvent()),
+          ),
+        ],
+      ),
     );
   }
 
@@ -122,22 +132,19 @@ class _RequestContainerState extends State<RequestContainer> with TickerProvider
                   height: 30,
                   child: Align(
                       alignment: Alignment.center,
-                      child: Text('Params', style: theme.textTheme.bodyText1)
-                  ),
+                      child: Text('Params', style: theme.textTheme.bodyText1)),
                 ),
                 SizedBox(
                   height: 30,
                   child: Align(
                       alignment: Alignment.center,
-                      child: Text('Headers', style: theme.textTheme.bodyText1)
-                  ),
+                      child: Text('Headers', style: theme.textTheme.bodyText1)),
                 ),
                 SizedBox(
                   height: 30,
                   child: Align(
                       alignment: Alignment.center,
-                      child: Text('Body', style: theme.textTheme.bodyText1)
-                  ),
+                      child: Text('Body', style: theme.textTheme.bodyText1)),
                 ),
               ],
             ),
@@ -145,6 +152,7 @@ class _RequestContainerState extends State<RequestContainer> with TickerProvider
           SizedBox(
             height: 200,
             child: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
               children: [
                 KeyValueTable(params: state.activeRequest.params),
                 KeyValueTable(params: state.activeRequest.headers),
@@ -168,12 +176,8 @@ class _RequestContainerState extends State<RequestContainer> with TickerProvider
         padding: EdgeInsets.only(top: 5),
         child: CupertinoScrollbar(
           controller: _responseTextScrollController,
-          child: SingleChildScrollView(
-            controller: _responseTextScrollController,
-            child: Text(state.activeRequest.response == ''
-                ? 'No Response'
-                : state.activeRequest.response),
-          ),
+          child: Text(
+              state.activeRequest.response == '' ? 'No Response' : state.activeRequest.response),
         ),
       ),
     );
